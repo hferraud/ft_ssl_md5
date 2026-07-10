11bd924d28c7879afac6c97364adc49f3a62acb6
hferraud <ferraudhugo@gmail.com>
2026-07-10 10:54:13 +0200

    feat: add md5 crypto algorithm

--- src/md5/crypto.h
+++ src/md5/crypto.h
@@ -5,6 +5,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-ft_ssl_status_t md5(uint8_t *message, size_t size);
+ft_ssl_status_t md5(uint8_t *message, size_t size, uint8_t digest[16]);
 
 #endif //FT_SSL_MD5_CRYPTO_H
--- src/md5/md5.c
+++ src/md5/md5.c
@@ -7,6 +7,7 @@
 #include "crypto.h"
 
 ft_ssl_status_t md5_handler(int argc, char **argv) {
+    uint8_t digest[16];
     md5_options_t options = {0};
 
     md5_parser(argc, argv, &options);
@@ -15,7 +16,12 @@ ft_ssl_status_t md5_handler(int argc, char **argv) {
     printf("r=%d\n", options.reverse);
     printf("s=%s\n", options.sum_input);
     if (options.sum_input != NULL) {
-        md5((uint8_t*)options.sum_input, strlen(options.sum_input));
+        md5((uint8_t*)options.sum_input, strlen(options.sum_input), digest);
     }
+    printf("Digest:\n");
+    for (size_t i = 0; i < 16; i++) {
+        printf("%02x", digest[i]);
+    }
+    printf("\n");
     return FT_SSL_OK;
 }
